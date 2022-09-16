9eb8ef394807de02700197deaa111e585d02ce4c
Sureshsahu7 <85033183+Sureshsahu7@users.noreply.github.com>
2022-09-17 00:55:40 +0530

    Add Dashboard ,List and Profile
    Used React-Chart-js-2 , moment , react-tooltip dependencies to design the dashboard and charts

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import { Routes, Route } from "react-router-dom"
 import Modal from "react-modal";
 import LandingPage from "./pages/LandingPage";
+import DashBoard from "./pages/DashBoard";
+import Main from './pages/Main';
 import { Scrollbars } from "react-custom-scrollbars";
 import Login from "./components/Login"
 import Signup from './components/Signup';
@@ -115,7 +117,28 @@ function App() {
               openModalLogin={openModalLogin}
               isLoggedIn={isLoggedIn}
               setIsLoggedIn={setIsLoggedIn} />} />
+              <Route
+            path="dashboard"
+            element={
+              <DashBoard
+                isLoggedIn={isLoggedIn}
+                setIsLoggedIn={setIsLoggedIn}
+                openModalExpense={openModalExpense}
+              />
+            }
+          />
+          <Route
+            path=""
+            element={
+              <Main
+                setDeleteId={setDeleteId}
+                openModalConfirm={openModalConfirm}
+                openModalBudget={openModalBudget}
+              />
+            }
+          />
         </Routes>
+        
         <Modal
           isOpen={modalIsOpen}
           onRequestClose={closeModalSignup}
@@ -153,6 +176,73 @@ function App() {
           </button>
           {/* <AddExpense closeModalExpense={closeModalExpense} /> */}
         </Modal>
+        <Modal
+        isOpen={modalIsOpenExpense}
+        onRequestClose={closeModalExpense}
+        style={customStyles}
+      >
+        <button onClick={closeModalExpense}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <AddExpense closeModalExpense={closeModalExpense} />
+      </Modal>
+      <Modal
+        isOpen={modalIsOpenBudget}
+        onRequestClose={closeModalBudget}
+        style={customStyles}
+      >
+        <button onClick={closeModalBudget}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <SetBudget closeModalBudget={closeModalBudget} />
+      </Modal>
+      <Modal
+        isOpen={modalisOpenConfirm}
+        onRequestClose={closeModalConfirm}
+        style={customStyles}
+      >
+        <button onClick={closeModalConfirm}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <ConfirmDelete
+          deleteId={deleteId}
+          closeModalConfirm={closeModalConfirm}
+        />
+      </Modal>
+
       </div>
     </Scrollbars >
 
